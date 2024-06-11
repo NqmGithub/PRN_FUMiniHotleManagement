5621046b85dd6d4a2e9a1ac128121a1e52fa27f9
Nam <nguyennqm2406@gmail.com>
2024-06-11 23:31:58 +0700

    initial commit

---
+++ FUMiniHotelSystem/WpfApp/ManageCustomerWindow.xaml.cs
@@ -0,0 +1,113 @@
+﻿using BusinessObjects;
+using Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp
+{
+    /// <summary>
+    /// Interaction logic for ManageCustomerWindow.xaml
+    /// </summary>
+    public partial class ManageCustomerWindow : Window
+    {
+        private readonly CustomerService customerService;
+        public ManageCustomerWindow()
+        {
+            InitializeComponent();
+            customerService = new CustomerService();
+            LoadData();
+        }
+
+        public void LoadData()
+        {
+            dtGridCustomer.ItemsSource = customerService.GetAll();
+        }
+
+        private void dtGridCustomer_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var selected = dtGridCustomer.SelectedItem as Customer;
+            if (selected != null)
+            {
+                txtID.Text = selected.CustomerId.ToString();
+                txtName.Text = selected.CustomerFullName.ToString();
+                txtPhone.Text = selected.Telephone.ToString();
+                txtEmail.Text = selected.EmailAddress.ToString();
+                txtBithday.Text = selected.CustomerBirthday.ToString();
+                txtStatus.Text = selected.CustomerStatus.ToString();
+                txtPassword.Text = selected.Password.ToString();
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Customer cus = new Customer();
+                cus.CustomerFullName = txtName.Text;
+                cus.EmailAddress = txtEmail.Text;
+                cus.CustomerBirthday = DateOnly.Parse(txtBithday.Text);
+                cus.CustomerStatus = byte.Parse(txtStatus.Text);
+                cus.Telephone = txtPhone.Text;
+                customerService.Add(cus);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                LoadData();
+            }
+        }
+
+        private void btnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Customer cus = new Customer();
+                cus.CustomerId = int.Parse(txtID.Text);
+                cus.CustomerFullName = txtName.Text;
+                cus.EmailAddress = txtEmail.Text;
+                cus.CustomerBirthday = DateOnly.Parse(txtBithday.Text);
+                cus.CustomerStatus = byte.Parse(txtStatus.Text);
+                cus.Telephone = txtPhone.Text;
+                customerService.Update(cus);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                LoadData();
+            }
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                customerService.Delete(int.Parse(txtID.Text));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                LoadData();
+            }
+        }
+    }
+}
---
+++ FUMiniHotelSystem/WpfApp/ManageRoomWindow.xaml.cs
@@ -0,0 +1,117 @@
+﻿using BusinessObjects;
+using Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+namespace WpfApp
+{
+    /// <summary>
+    /// Interaction logic for ManageRoomWindow.xaml
+    /// </summary>
+    public partial class ManageRoomWindow : Window
+    {
+        private readonly RoomInformationService roomService;
+        public ManageRoomWindow()
+        {
+            InitializeComponent();
+            roomService = new RoomInformationService();
+            LoadData();
+        }
+
+        public void LoadData()
+        {
+            dtGridRoom.ItemsSource = roomService.GetAll();
+        }
+
+        private void dtGridRoom_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var selected = dtGridRoom.SelectedItem as RoomInformation;
+            if (selected != null)
+            {
+                txtID.Text = selected.RoomId.ToString();
+                txtStatus.Text = selected.RoomStatus.ToString();
+                txtCapacity.Text = selected.RoomMaxCapacity.ToString();
+                txtDescription.Text = selected.RoomDetailDescription;
+                txtNumber.Text = selected?.RoomNumber.ToString();
+                txtPrice.Text = selected.RoomPricePerDay.ToString();
+                txtType.Text = selected?.RoomTypeId.ToString();
+            }
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                RoomInformation room = new RoomInformation();
+                room.RoomMaxCapacity = int.Parse(txtCapacity.Text);
+                room.RoomDetailDescription = txtDescription.Text;
+                room.RoomNumber = txtNumber.Text;
+                room.RoomPricePerDay = Decimal.Parse(txtPrice.Text);
+                room.RoomStatus = byte.Parse(txtStatus.Text);
+                room.RoomTypeId = int.Parse(txtType.Text);
+
+                roomService.Add(room);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                LoadData();
+            }
+        }
+
+        private void btnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                RoomInformation room = new RoomInformation();
+                room.RoomMaxCapacity = int.Parse(txtCapacity.Text);
+                room.RoomDetailDescription = txtDescription.Text;
+                room.RoomNumber = txtNumber.Text;
+                room.RoomPricePerDay = Decimal.Parse(txtPrice.Text);
+                room.RoomStatus = byte.Parse(txtStatus.Text);
+                room.RoomTypeId = int.Parse(txtType.Text);
+
+                roomService.Update(room);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                LoadData();
+            }
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                roomService.Delete(int.Parse(txtID.Text));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                LoadData();
+            }
+        }
+    }
+}
